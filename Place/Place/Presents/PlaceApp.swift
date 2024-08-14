7e4bfaeaf4f0bad7939d4f60703d87db99642428
MinhoLee <bluemango608@gmail.com>
2024-08-14 16:31:51 +0900

    project start

---
+++ Place/Place/Presents/PlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceApp.swift
+//  Place
+//
+//  Created by 이민호 on 8/14/24.
+//
+
+import SwiftUI
+
+@main
+struct PlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
